7352dd48f7e6c4037c2c65160788decb030bdc79
Merly2004 <1595077@senati.pe>
2026-06-11 10:38:58 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://bd_seguridad_epp_user:8OdHgv8EKafcMPTnNUfWvLnu55SwBFRk@dpg-d8bsno58nd3s738v15i0-a.oregon-postgres.render.com:5432/bd_seguridad_epp',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
